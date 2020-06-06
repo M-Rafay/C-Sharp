9efe70eee42a20ec737bcace77ba38dc066da63f
M-Rafay <real.muhammadrafay@gmail.com>
2020-06-06 21:08:59 +0500

    C# Learning added oop

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOP.com.studentManagement;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std = new com.studentManagement.Student();
+            std.age = 12;
+            std.name = "Rafay" ;
+            std.marks = 100;
+
+            std.detail();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP/OOP/com/studentManagement/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.com.studentManagement
+{
+    class Student
+    {
+        public int age;
+        public int marks;
+        public string name;
+
+        public void detail()
+        {
+            Console.WriteLine("Name = {0} Age = {1} Marks = {2}",name,age,marks);
+        }
+    }
+}
